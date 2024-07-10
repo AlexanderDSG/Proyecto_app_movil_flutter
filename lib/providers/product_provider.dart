63f1c9b44d0f3182cc703ca58f60a6bd30cef3b4
alexandersuntaxi20030414 <alexandersuntaxi20030414@gmail.com>
2024-07-10 23:49:24 +0000

    update,delete

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -31,3 +31,72 @@ final productByIdProvider = FutureProvider.family<Product, String>((ref, id) asy
 
   return product;
 });
+
+//actualizar
+final productUpdateProvider = StateNotifierProvider<ProductUpdateNotifier, bool>((ref) {
+  final dio = ref.watch(dioProvider);
+  return ProductUpdateNotifier(dio);
+});
+
+class ProductUpdateNotifier extends StateNotifier<bool> {
+  final Dio _dio;
+
+  ProductUpdateNotifier(this._dio) : super(false);
+
+  Future<bool> updateProduct(Product product) async {
+    try {
+      final response = await _dio.patch(
+        "https://pucei.edu.ec:9101/api/v2/products/${product.id}",
+        data: product.toJson(),
+      );
+
+      if (response.statusCode == 200) {
+        state = true;
+        return true;
+      } else {
+        print('Error: ${response.statusCode} - ${response.data}');
+        state = false;
+        return false;
+      }
+    } catch (e) {
+      print('Exception: $e');
+      state = false;
+      return false;
+    }
+  }
+}
+
+//eliminar
+final productDeleteProvider = StateNotifierProvider<ProductDeleteNotifier, bool>((ref) {
+  final dio = ref.watch(dioProvider);
+  return ProductDeleteNotifier(dio);
+});
+
+class ProductDeleteNotifier extends StateNotifier<bool> {
+  final Dio _dio;
+
+  ProductDeleteNotifier(this._dio) : super(false);
+
+  Future<bool> deleteProduct(String productId) async {
+    try {
+      final response = await _dio.delete(
+        "https://pucei.edu.ec:9101/api/v2/products/$productId",
+      );
+
+      if (response.statusCode == 200) {
+        state = true;
+        return true;
+      } else {
+        print('Error: ${response.statusCode} - ${response.data}');
+        state = false;
+        return false;
+      }
+    } catch (e) {
+      print('Exception: $e');
+      state = false;
+      return false;
+    }
+  }
+}
+
+
